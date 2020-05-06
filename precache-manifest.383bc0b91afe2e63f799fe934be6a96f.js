8b979f7cab8ef0e674b6035f02c612101ac5c7c6
kelbikhanov <rusik9091@gmail.com>
2020-05-07 00:17:12 +0300

    Updates

---
+++ precache-manifest.383bc0b91afe2e63f799fe934be6a96f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21be85afa8bd541cf0ef8401336abe11",
+    "url": "/ChatTest/index.html"
+  },
+  {
+    "revision": "d60387c530724be8d0d3",
+    "url": "/ChatTest/static/css/main.1e55fa60.chunk.css"
+  },
+  {
+    "revision": "f47f01a48926762c8a83",
+    "url": "/ChatTest/static/js/2.8c02d964.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/ChatTest/static/js/2.8c02d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60387c530724be8d0d3",
+    "url": "/ChatTest/static/js/main.5f765e77.chunk.js"
+  },
+  {
+    "revision": "22be6cb22df991943324",
+    "url": "/ChatTest/static/js/runtime-main.a8e04bea.js"
+  }
+]);
